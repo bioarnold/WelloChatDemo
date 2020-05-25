c2d59f3ccfada3910c1f342ef7a7d257b1105717
Peter Neumann <peter.neumann@dreamlines.com>
2020-05-25 19:48:57 +0200

    Adding swagger

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,33 @@
 const userRepository = require('./../dataAccess/userRepository');
 
-// Display book create form on GET.
+/**
+ * @swagger
+ * definitions:
+ *   User:
+ *     properties:
+ *       userName:
+ *         type: string
+ *       email:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *       - Users
+ *     description: Returns all users
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of users
+ *         schema:
+ *           type: array
+ *           items:
+ *             "$ref": '#/definitions/User'
+ */
 exports.getUsers = async function (req, res) {
     try {
         res.send(await userRepository.getUsers());
--- src/index.js
+++ src/index.js
@@ -1,9 +1,56 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const port = 8000;
+
+// swagger definition
+var swaggerDefinition = {
+    info: {
+        title: 'WelloChat Demo API',
+        version: '1.0.0',
+        description: 'WelloChat Demo API',
+    },
+    host: `localhost:${port}`,
+    basePath: '/',
+};
+
+// options for the swagger docs
+var options = {
+    // import swaggerDefinitions
+    swaggerDefinition: swaggerDefinition,
+    // path to the API docs
+    apis: ['./src/controllers/*.js'],
+};
+
+// initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
+
+app.get('/swagger.json', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
+app.use(
+    '/swagger',
+    (req, res, next) => {
+        const opts = {
+            swaggerOptions: {
+                urls: [{ url: '/swagger.json', name: 'swagger.json' }],
+            },
+        };
+
+        generatedHtml = swaggerUi.generateHTML(null, opts);
+        next();
+    },
+    swaggerUi.serve,
+    (req, res) => res.send(generatedHtml)
+);
 
 app.use('/', routes);
 
-app.listen(8000, () => {
+app.listen(port, () => {
     console.log('Server listening on port 8000!');
 });
